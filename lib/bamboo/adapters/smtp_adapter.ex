bc4837cf6e0c91d65cf8a2277f40eeedb8b35e8c
Igor Shinkarenko <igor.shinkarenko@swtecnn.com>
2020-08-19 22:30:42 +0300

    Add cid field to attachment part

--- lib/bamboo/adapters/smtp_adapter.ex
+++ lib/bamboo/adapters/smtp_adapter.ex
@@ -203,8 +203,9 @@ defmodule Bamboo.SMTPAdapter do
     << random :: size(32) >> = :crypto.strong_rand_bytes(4)
     body
     |> add_smtp_line("Content-Type: #{attachment.content_type}; name=\"#{attachment.filename}\"")
-    |> add_smtp_line("Content-Disposition: attachment; filename=\"#{attachment.filename}\"")
+    |> add_smtp_line("Content-Disposition: inline; filename=\"#{attachment.filename}\"")
     |> add_smtp_line("Content-Transfer-Encoding: base64")
+    |> add_smtp_line("Content-ID: <#{attachment.content_id}>")
     |> add_smtp_line("X-Attachment-Id: #{random}")
   end
 
